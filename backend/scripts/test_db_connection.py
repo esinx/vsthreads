84abf8a7cbdb7efc85d3d9c22cc5e09d9f15e841
Jesse Zong <jessej.zong@gmail.com>
2024-03-30 00:53:39 -0400

    Got base connections to db working

---
+++ backend/scripts/test_db_connection.py
@@ -0,0 +1,17 @@
+from pymongo.mongo_client import MongoClient
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+uri = os.environ["MONGO_URI_DEV"]
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command("ping")
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from pymongo.mongo_client import MongoClient
+from motor import motor_asyncio
+import os
+
 
 app = FastAPI()
+print(os.environ["MONGO_URI_DEV"])
+client = motor_asyncio.AsyncIOMotorClient(os.environ["MONGO_URI_DEV"])
+db = client.get_database("vsthreads")
+user_collection = db.get_collection("users")
+thread_collection = db.get_collection("threads")
+# client = MongoClient(os.environ["MONGO_URI_DEV"])
 
 
 @app.get("/")
