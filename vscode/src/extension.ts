4dc09f4dc9ebf26d5ad0661e958597644b84bdd8
Eunsoo Shin <me@esinx.net>
2024-03-30 10:24:09 -0400

    🚧 wip(vsc): poc extension

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,26 +1,87 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
+import { parseComments } from "./parser"
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let threads: Record<string, vscode.CommentThread> = {}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vsthreads" is now active!');
+class Thread implements vscode.Comment {
+	id: string
+	label: string
+	body: string | vscode.MarkdownString = new vscode.MarkdownString(`
+	\`\`\`typescript
+   const parser = parseComments(activeEditor.document.languageId)
+	\`\`\`
+   `)
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vsthreads.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vsthreads!');
-	});
-
-	context.subscriptions.push(disposable);
+	mode: vscode.CommentMode = vscode.CommentMode.Preview
+	author: vscode.CommentAuthorInformation = {
+		name: "VS Threads",
+	}
+	constructor(id: string) {
+		this.id = id
+		this.label = `@thread:${id}`
+	}
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function activate(context: vscode.ExtensionContext) {
+	if (!vscode.window.activeTextEditor) {
+		return
+	}
+	const activeEditor = vscode.window.activeTextEditor
+	const commentController = vscode.comments.createCommentController(
+		"vsthreads",
+		"vsthreads"
+	)
+	context.subscriptions.push(commentController)
+
+	commentController.commentingRangeProvider = {
+		provideCommentingRanges: (document, token) => {
+			const lineCount = document.lineCount
+			return [new vscode.Range(0, 0, lineCount - 1, 0)]
+		},
+	} satisfies vscode.CommentingRangeProvider
+	context.subscriptions.push(
+		vscode.commands.registerCommand(
+			"vsthreads.createThread",
+			(reply: vscode.CommentReply) => {}
+		)
+	)
+
+	const parser = parseComments(activeEditor.document.languageId)
+
+	const renderComments = async () => {
+		const editorText = activeEditor.document.getText()
+		const comments = await parser(editorText)
+		let removedThreads = new Set(Object.keys(threads))
+		comments.forEach((comment) => {
+			const match = comment.content.match(/@thread:(.+)/)
+			if (!match) {
+				return
+			}
+			const threadId = match[1]
+			if (!threads[threadId]) {
+				const thread = commentController.createCommentThread(
+					vscode.Uri.file(activeEditor.document.fileName),
+					comment.range,
+					[]
+				)
+				thread.comments = [new Thread(threadId)]
+				threads[threadId] = thread
+			}
+			removedThreads.delete(threadId)
+		})
+		removedThreads.forEach((threadId) => {
+			threads[threadId].dispose()
+			threads = Object.fromEntries(
+				Object.entries(threads).filter(([id]) => id !== threadId)
+			)
+		})
+	}
+
+	vscode.workspace.onDidChangeTextDocument(
+		async (event) => renderComments(),
+		null,
+		context.subscriptions
+	)
+
+	renderComments()
+}
